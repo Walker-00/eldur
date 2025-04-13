df2435d92cac81f1a646806dbd95cc213737e821
Linus Walker <rissk_it@proton.me>
2025-04-13 15:20:15 +0630

    chore: Tensor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Deref};
+
 use ndarray::{ArrayD, IxDyn};
 use num_traits::{One, Zero};
 
@@ -5,7 +7,18 @@ pub struct Tensor<T> {
     pub data: ArrayD<T>,
 }
 
-impl<T: Clone + Zero + One> Tensor<T> {
+impl<T> Deref for Tensor<T> {
+    type Target = ArrayD<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> Tensor<T>
+where
+    T: Clone + Zero + One,
+{
     pub fn from_array(data: ArrayD<T>) -> Self {
         Self { data }
     }
@@ -28,6 +41,25 @@ impl<T: Clone + Zero + One> Tensor<T> {
 
     pub fn get(&self, index: &[usize]) -> Option<&T> {
         self.data.get(index)
+    }
+
+    pub fn set(&mut self, index: &[usize], value: T) {
+        if let Some(elem) = self.data.get_mut(index) {
+            *elem = value;
+        }
+    }
+}
+
+impl<T> Add for Tensor<T>
+where
+    T: Clone + Zero + One,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            data: self.data + rhs.data,
+        }
     }
 }
 
