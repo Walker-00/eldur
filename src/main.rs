9c9aa4a612d9bbcd34cddaa44407bab81f573917
Linus Walker <rissk_it@proton.me>
2025-04-13 14:14:25 +0630

    chore: Tensor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use ndarray::{ArrayD, IxDyn};
+use num_traits::{One, Zero};
+
+pub struct Tensor<T> {
+    pub data: ArrayD<T>,
+}
+
+impl<T: Clone + Zero + One> Tensor<T> {
+    pub fn from_array(data: ArrayD<T>) -> Self {
+        Self { data }
+    }
+
+    pub fn zeros(shape: &[usize]) -> Self {
+        Self {
+            data: ArrayD::zeros(IxDyn(shape)),
+        }
+    }
+
+    pub fn ones(shape: &[usize]) -> Self {
+        Self {
+            data: ArrayD::ones(IxDyn(shape)),
+        }
+    }
+
+    pub fn shape(&self) -> &[usize] {
+        self.data.shape()
+    }
+
+    pub fn get(&self, index: &[usize]) -> Option<&T> {
+        self.data.get(index)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
