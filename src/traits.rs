9710a6c30172d915dddcf49762781af35f74f129
Linus Walker <rissk_it@proton.me>
2025-11-21 20:53:44 +0630

    feat: Par Tensor Operations trait structure

--- src/traits.rs
+++ src/traits.rs
@@ -1,13 +1,27 @@
-pub trait SeqTensorOps {
-    type SeqTensor: SeqTensorOps;
+pub trait TensorOps {
+    type Tensor: TensorOps;
 
-    fn add(&self, other: Self::SeqTensor) -> Self::SeqTensor;
+    fn add(&self, other: Self::Tensor) -> Self::Tensor;
 
     fn sum(&self) -> f32;
 
     fn mean(&self) -> f32;
 
-    fn swapaxes(&self, axis1: usize, axis2: usize) -> Self::SeqTensor;
+    fn swapaxes(&self, axis1: usize, axis2: usize) -> Self::Tensor;
 
-    fn narrow(&self, axis: usize, start: usize, end: usize) -> Self::SeqTensor;
+    fn narrow(&self, axis: usize, start: usize, end: usize) -> Self::Tensor;
+}
+
+pub trait ParTensorOps {
+    type Tensor: TensorOps;
+
+    fn par_add(&self, other: Self::Tensor) -> Self::Tensor;
+
+    fn par_sum(&self) -> f32;
+
+    // fn mean(&self) -> f32;
+    //
+    // fn swapaxes(&self, axis1: usize, axis2: usize) -> Self::Tensor;
+    //
+    // fn narrow(&self, axis: usize, start: usize, end: usize) -> Self::Tensor;
 }
